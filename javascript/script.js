9d118ee2e7ba00178709d5abcbe9db56861188ed
jaumerella <jaumerella@gmail.com>
2015-05-26 11:14:15 +0200

    modificaciones
    He actualizado el fichero javascript y ya hace bien las consultas para
    recoger info de los cursos. Tambien he embebido un mapa de google maps
    en location

--- javascript/script.js
+++ javascript/script.js
@@ -2,23 +2,23 @@ $(document).ready(ready);
 
 function ready(){
     console.log("I'm ready!");
-    var id=1;
+    var course_id=1;
     
     $.ajax({
         method: "POST",
         //dataType: "json", //type of data
-        crossDomain: true, //localhost purposes
-        url: "php/sql_connection.php", //Relative or absolute path to file.php file
-        data: {course:id},
+        //crossDomain: true, //localhost purposes
+        url: "../php/sql_connection.php", //Relative or absolute path to file.php file
+        data: {course:course_id},
         success: function(response) {
             console.log(JSON.parse(response));
             var courses=JSON.parse(response);
             var el="";
             for(var i=0;i<courses.length;i++){
-                console.log(courses[i].title);
+                console.log(courses[i].name);
                 
-                el+="<div class='course' id='c"+courses[i].id+"'><h2>"+courses[i].title+"</h2><span>"+courses[i].description+"</span></div>";             
-                
+                el+="<h2>"+courses[i].name+"</h2><span>"+courses[i].description+"</span>";   
+
             }
             
             $(".courses").append(el);
--- php/sql_connection.php
+++ php/sql_connection.php
@@ -1,10 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 //get all the course from db and reply using json structure
  
 //echo "I'm the php";
  
 //connection to db
-$mysqli = new mysqli("localhost", "root", "", "biggym");
+$mysqli = new mysqli("localhost", "ibericusgym", "", "my_ibericusgym");
  
 if (mysqli_connect_errno()) { //verify connection
     echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
